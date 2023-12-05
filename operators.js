b1ff09e99c68d2b4bab745f61301e0b2eee36bca
podestal <luisrodriguezpodesta@gmail.com>
2023-12-05 18:43:50 -0500

    operators

---
+++ operators.js
@@ -0,0 +1,24 @@
+// Logical operators in JS:
+// && and
+// || or
+// ! not
+
+// Arithmetic operators in JS:
+// + addition
+// - substraction
+// * multiplication
+// / division
+
+// Comparison operators in JS:
+// > greater than
+// < less than
+// == equal
+
+console.log(2 + 2);
+console.log(1 + 2 + 3 + 4 + 5);
+console.log(20 - 18);
+console.log(2 * 3);
+console.log(8 / 1);
+console.log(3 > 2);
+console.log(2 > 3);
+console.log(10 == 10);
