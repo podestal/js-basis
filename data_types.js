4783a5892f3c9a27bb0f912fce9f477a2b7f48ec
podestal <luisrodriguezpodesta@gmail.com>
2023-12-05 18:34:23 -0500

    data types

---
+++ data_types.js
@@ -0,0 +1,13 @@
+// DATA TYPES
+// String  
+// Number
+// Boolean
+// Null
+// Undefined
+// BigInt
+// Symbol
+
+var title = 'Generic Title'
+var description = 'Generic Description'
+var prices = 5.70
+var international = true
---
+++ variables.js
@@ -0,0 +1,8 @@
+var person = 'John'
+var greeting = 'Hello'
+console.log(greeting, person);
+greeting = 'Hi'
+person = 'James'
+console.log(greeting, person);
+var petDog = 'Rex'; // Task 1 solution
+console.log(petDog);
