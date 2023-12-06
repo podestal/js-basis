7147aa46f85689f602054845e7c68ec4fc70cdaf
podestal <luisrodriguezpodesta@gmail.com>
2023-12-05 19:13:21 -0500

    selectors

---
+++ selectors.js
@@ -0,0 +1,10 @@
+// Returns the first p element
+document.querySelector('p')
+// Returns the first a element
+document.querySelector('a')
+// Returns all the p elements
+document.querySelectorAll('p')
+// Return the elment by id
+document.getElementById('heading')
+// // Return the elment by class
+document.getElementsByClassName('txt')
