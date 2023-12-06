7f27d8e66be37351e294ac7df5c1027f395a7a04
podestal <luisrodriguezpodesta@gmail.com>
2023-12-05 19:18:03 -0500

    functions

---
+++ functions.js
@@ -0,0 +1,17 @@
+function addTwoNumbers() {
+    var a = 10
+    var b = 20
+    var c = a + b
+    console.log(c);
+}
+
+addTwoNumbers()
+
+// Fnx with parameters
+
+function addTwoNumbersP(a, b) {
+    var c = a + b
+    console.log(c);
+}
+
+addTwoNumbersP(2, 2)
